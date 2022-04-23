2d0a1784adfd7b10d3e37697dcc6f0e75258ca56
EoinWhelanDaev <eoinfar75@gmail.com>
2022-04-23 05:05:55 +0100

    Inital + final commit for Sovran logger.

---
+++ Sovran.Logger/ISovranLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace Sovran.Logger
+{
+    /// <summary>
+    /// Main contract/interface for Sovran Logger.
+    /// </summary>
+    public interface ISovranLogger
+    {
+        void LogPayload(object payload);
+        void LogActivity(string message);
+        void LogError(string message);
+    }
+}
